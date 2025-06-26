57bedf7732d70b4a080932725ee3b7b5291f0f46
krisha-thummar <krishathummar96@gmail.com>
2025-06-25 19:22:06 -0700

    Add Day 1 - variables and data types

---
+++ day 1/variables.py
@@ -0,0 +1,16 @@
+# Day 1 - Python Basics: Variables and Data Types
+
+name = "Krisha"
+age = 21
+height = 5.7
+is_learning = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Learning Python:", is_learning)
+
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_learning:", type(is_learning))
