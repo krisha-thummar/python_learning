8825283f422c6a63d45a797e11b28710fe327639
krisha-thummar <krishathummar96@gmail.com>
2025-07-05 22:34:08 -0700

    Add Day 2: operators,strings and math functions

---
+++ day 2/formatted_strings.py
@@ -0,0 +1,8 @@
+name = "krisha"
+age = 21
+
+#using f-string
+print(f"Hello!, My name is {name}. I am {age} years old. ")
+
+#using format
+print("hello!, my name is {}. I am {} years old.".format(name,age))
---
+++ day 2/str_methods.py
@@ -0,0 +1,20 @@
+sentence = input("what is your mother's name?")
+print(sentence)
+
+print(type(sentence))
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.title())
+
+line = "Hello krisha!"
+print(line.strip())
+print(line.split())
+print(line.find('krisha'))
+print(line.isalnum())
+print(line.isalpha())
+print(len(line))
+print("hello" in line)
+
+name= "akshay"
+print(name.replace("a","A"))
